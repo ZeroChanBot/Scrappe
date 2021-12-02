9cf04edd748b90b870d916180da9ff5520cdf589
Nathanael Hananta Jevano <88374559+ZeroChanBot@users.noreply.github.com>
2021-12-02 14:48:22 +0700

    Create iplookup.js

---
+++ iplookup.js
@@ -0,0 +1,15 @@
+let node = require("node-fetch")
+
+module.exports = async (query) => {
+return new Promise(async (resolve, reject) => {
+try {
+var res = await node("http://ip-api.com/json/"+query+"?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query")
+let json = await res.json()
+if (json.status !== 'success') return reject({ status: 404, message: 'Invalid query', query }) 
+json.status = 200
+resolve(json)
+} catch (e) {
+reject({ status: 500, message: 'Internal Server Error!' })
+}
+})
+}
